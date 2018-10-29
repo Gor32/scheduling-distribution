97b0af6c8b9ca6faf21eb7e15ece33d43498aa0c
Gor32 <kirakosyan0001@gmail.com>
2018-10-29 19:59:48 +0400

    config(index): initialize config

---
+++ src/config/index.js
@@ -0,0 +1,9 @@
+const host = '//localhost:5000'
+
+const API_PATH = `${host}`
+
+export const ENDPOINTS = {
+  GET_ALL_EDUCATIONAL_PLAN: `${API_PATH}/educationalPlan`,
+  CREATE_EDUCATIONAL_ROW: `${API_PATH}/educationalPlan`
+}
+
