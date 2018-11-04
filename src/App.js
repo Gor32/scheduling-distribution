26c4599f479745c3def150e3e698fdb19f07e6af
Gor32 <kirakosyan0001@gmail.com>
2018-11-04 14:10:33 +0400

    root(package): added react-router for navigation components
    components(src): added Router and Link in components. Style is very bad (hasn't) must be change

--- src/App.js
+++ src/App.js
@@ -1,13 +1,34 @@
 import React, { Component } from 'react'
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom'
+
 import './App.css'
+
 import EducationalPlan from './components/educationalPlan/EducationalPlan'
+import Home from './components/home/Home'
+import Subjects from './components/subjects/Subjects'
 
 class App extends Component {
   render () {
     return (
-      <div className="App">
-        <EducationalPlan></EducationalPlan>
-      </div>
+      <Router>
+        <div className="App">
+          <ul>
+            <li>
+              <Link to="/">Home </Link>
+            </li>
+            <li>
+              <Link to="/educationalPlan">Educational PLan</Link>
+            </li>
+            <li>
+              <Link to="/subjects">Subjects</Link>
+            </li>
+          </ul>
+          <hr/>
+          <Route exact path='/' component={Home}/>
+          <Route path='/educationalPlan' component={EducationalPlan}/>
+          <Route path='/subjects' component={Subjects}/>
+        </div>
+      </Router>
     )
   }
 }
