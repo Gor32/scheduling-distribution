14933df192475566705cb39e8cfe058c7ef1a1c4
Gor32 <kirakosyan0001@gmail.com>
2019-02-23 11:35:08 +0400

    model(classifier): added new column number of students

--- server/api/models/classifiersSchema.js
+++ server/api/models/classifiersSchema.js
@@ -3,7 +3,8 @@ const mongoose = require('mongoose')
 const classifiersSchema = mongoose.Schema({
   _id: mongoose.Schema.Types.ObjectId,
   classifier: String,
-  group: String
+  group: String,
+  numberOfStudents: Number
 })
 
 module.exports = mongoose.model('Classifiers', classifiersSchema)
